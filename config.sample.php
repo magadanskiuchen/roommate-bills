6b60e5f7f75bf40254e144ea5847f2c0da649ec7
Georgi Popov <magadanski_uchen@yahoo.com>
2016-09-17 00:09:57 +0300

    added sample MySQL DB constants

--- config.sample.php
+++ config.sample.php
@@ -1,3 +1,11 @@
 <?php
-define('RB_DB_CLASS', '');
+# define('RB_DB_CLASS', '');
+
+# MySQL set-up constants
+# define('RB_MYSQL_HOST', '');
+# define('RB_MYSQL_USER', '');
+# define('RB_MYSQL_PASS', '');
+# define('RB_MYSQL_PORT', '');
+# define('RB_MYSQL_SOCKET', '');
+# define('RB_MYSQL_DB', '');
 ?>
